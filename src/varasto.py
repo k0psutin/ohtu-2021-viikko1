0d86334f92abad6e18b6852ad019066bb3a0275b
k0psutin <mr.gravez@gmail.com>
2021-01-21 14:06:14 +0200

    Fix trailing whitespaces

--- src/varasto.py
+++ src/varasto.py
@@ -5,10 +5,11 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
+
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
@@ -31,7 +32,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +41,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self): # pragma: no cover
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
